a2c6578e04b9b5d77fbce32eb6c97d39fecc748d
Vasilut Lucian <lucian.vasilut10@gmail.com>
2017-11-05 16:17:08 +0200

    Added Interface Principle example.

---
+++ SOLID/SOLID/Interface principle/ProblemInterfacePrinciple.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.Interface_principle
+{
+    /*
+     * Problem: we need to add a new operation (Read operation).
+    Looking to the Liskov solution, we have the IOperation interface. If we'll add the Read operation a lot of customers will be impacted.
+    */
+    public interface IOperationBad
+    {
+        void Add();
+        void Read(); //operation for the new client
+    }
+}
---
+++ SOLID/SOLID/Interface principle/SolutionInterfacePrinciple.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.Interface_principle
+{
+    /*
+     * The solution is to create a new interface for the clients that demand the new interface
+     * so, we'll have the IOperation interface with the Add method
+     * we'll create a new interfrace IOperationExtended which extend the IOperation for clients who whants Read method.
+     */
+
+    public interface IOperation
+    {
+        void Add();
+    }
+
+    public interface IOperationExtended : IOperation
+    {
+        void Read();
+    }
+
+    public class ClientWithRead : IOperationExtended
+    {
+        public void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Read()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class NormalClient : IOperation
+    {
+        public void Add()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ExecutionInterface
+    {
+        public void Execute()
+        {
+            IOperation op = new NormalClient(); //client with add operation
+            IOperationExtended opExtended = new ClientWithRead();
+        }
+    }
+
+}
