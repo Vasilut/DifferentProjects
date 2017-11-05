c9a22bfeda527594ea95c3e55274405e8599bfeb
Vasilut Lucian <lucian.vasilut10@gmail.com>
2017-11-05 15:20:19 +0200

    Added Open close principle example.

---
+++ SOLID/SOLID/Open closed principle/ProblemOcp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.Open_closed_principle
+{
+    /*
+    The problem is if we add a new customer type we need 
+    to go and add one more “IF” condition in the “GetMoney” function, in other words we need to change the client class.
+
+    In other words we are “MODIFYING” the current client code for every change
+    and every time we modify we need to ensure that all the previous functionalities and connected client are working as before.
+     */
+    public class Client
+    {
+        private int _clientType;
+        public int ClientType
+        {
+            get
+            {
+                return _clientType;
+            }
+            set
+            {
+                _clientType = value;
+            }
+        }
+
+        public int GetMoney(int totalAmount)
+        {
+            if(ClientType == 1)
+            {
+                return totalAmount - 100;
+            }
+            else
+            {
+                return totalAmount - 50;
+            }
+        }
+    }
+}
---
+++ SOLID/SOLID/Open closed principle/SolutionOcp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.Open_closed_principle
+{
+    /*
+     *  In other words every time a new client type needs to be added
+     *   we create a new class as shown in the below. So whatever is the current code they are untouched and we just need to test and check the new classes.
+     *   
+     *   The ClientGood class is now closed for any new modification but it's open for extension
+    */
+    public class ClientGood
+    {
+        public virtual int GetMoney(int totalAmount)
+        {
+            return totalAmount;
+        }
+    }
+
+    public class GoldClient:ClientGood
+    {
+        public override int GetMoney(int totalAmount)
+        {
+            return base.GetMoney(totalAmount) - 100;
+        }
+    }
+
+    public class SilverClient:ClientGood
+    {
+        public override int GetMoney(int totalAmount)
+        {
+            return base.GetMoney(totalAmount) - 50;
+        }
+    }
+}
