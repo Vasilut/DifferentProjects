067dc99451044b1a4a2ffbfac897e541131baf77
Vasilut Lucian <lucian.vasilut10@gmail.com>
2017-11-05 15:07:44 +0200

    Added SingleResponsabilityPrinciple example.

---
+++ SOLID/SOLID/SingleResponsabilityPrinciple/Problem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.SingleResponsabilityPrinciple
+{
+    //this class should not doing log activity, so its over loaded with lot of responsability
+    public class Customer
+    {
+        public void Add()
+        {
+            try
+            {
+                //operations
+            }
+            catch(Exception ex)
+            {
+                File.WriteAllText(@"file.txt", ex.ToString()); //here is the problem
+            }
+        }
+    }
+}
---
+++ SOLID/SOLID/SingleResponsabilityPrinciple/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.SingleResponsabilityPrinciple
+{
+    public class CustomerGood
+    {
+        private FileLogger _fileLogger;
+        public CustomerGood()
+        {
+            _fileLogger = new FileLogger();
+        }
+        public void Add()
+        {
+            try
+            {
+                //operations
+            }
+            catch (Exception ex)
+            {
+                _fileLogger.Handle(ex.ToString()); //now the logging is handled in the FileLogger class.
+            }
+        }
+    }
+
+    public class FileLogger
+    {
+        public void Handle(string error)
+        {
+            File.WriteAllText(@"file.txt", error);
+        }
+    }
+}
