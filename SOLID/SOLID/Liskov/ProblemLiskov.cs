2397c0cb9e88122b5b05e0c0409948f5aac51153
Vasilut Lucian <lucian.vasilut10@gmail.com>
2017-11-05 15:59:58 +0200

    Added Liskov example.

---
+++ SOLID/SOLID/Liskov/ProblemLiskov.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.Liskov
+{
+    /*
+     * LISKOV principle says the parent should easily replace the child object
+     * For PowerUser this is not hapenning!!!!
+     */
+    public class LiskovBad
+    {
+        public virtual void Add()
+        {
+            Console.WriteLine("Base add");
+        }
+
+        public virtual int GetMoney(int totalAmount)
+        {
+            return totalAmount;
+        }
+    }
+
+    public class GoldCustomer : LiskovBad
+    {
+        public override void Add()
+        {
+            Console.WriteLine("Gold add");
+        }
+
+        public override int GetMoney(int totalAmount)
+        {
+            return base.GetMoney(totalAmount) - 100;
+        }
+    }
+
+    public class SilverCustomer : LiskovBad
+    {
+        public override void Add()
+        {
+            Console.WriteLine("Silver add");
+        }
+
+        public override int GetMoney(int totalAmount)
+        {
+            return base.GetMoney(totalAmount) - 50;
+        }
+    }
+
+    public class PowerCustomer : LiskovBad
+    {
+        public override void Add()
+        {
+            throw new Exception("Not alowed");
+        }
+
+        public override int GetMoney(int totalAmount)
+        {
+            return base.GetMoney(totalAmount) - 80;
+        }
+    }
+
+    public class Execution
+    {
+        public void Execute()
+        {
+            List<LiskovBad> lst = new List<LiskovBad>();
+            lst.Add(new GoldCustomer());
+            lst.Add(new SilverCustomer());
+            lst.Add(new PowerCustomer());
+
+            foreach (var item in lst)
+            {
+                item.Add(); //problem for the power user object
+            }
+        }
+    }
+}
---
+++ SOLID/SOLID/Liskov/SolutionLiskov.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.Liskov
+{
+    //we're separating the objects, in two categories: subclass of ListkovGood base class and the other.
+    public interface IMoney
+    {
+        int GetMoney(int totalAmount);
+    }
+
+    public interface IOperation
+    {
+        void Add();
+    }
+
+    public class LiskovGood : IMoney, IOperation
+    {
+        public virtual void Add()
+        {
+            
+        }
+
+        public virtual int GetMoney(int totalAmount)
+        {
+            return totalAmount;
+        }
+    }
+
+    public class GoldCustomerGood : LiskovGood
+    {
+        public override void Add()
+        {
+            Console.WriteLine("Gold add");
+        }
+
+        public override int GetMoney(int totalAmount)
+        {
+            return base.GetMoney(totalAmount) - 100;
+        }
+    }
+
+    public class SilverCustomerGood : LiskovGood
+    {
+        public override void Add()
+        {
+            Console.WriteLine("Silver add");
+        }
+
+        public override int GetMoney(int totalAmount)
+        {
+            return base.GetMoney(totalAmount) - 50;
+        }
+    }
+
+    public class PowerCustomerGood : IMoney
+    {
+        
+        public  int GetMoney(int totalAmount)
+        {
+            return totalAmount - 80;
+        }
+    }
+
+    public class ExecutionGood
+    {
+        public void Execute()
+        {
+            List<LiskovGood> lst = new List<LiskovGood>();
+            lst.Add(new GoldCustomerGood());
+            lst.Add(new SilverCustomerGood());
+            //lst.Add(new PowerCustomerGood());
+
+            foreach (var item in lst)
+            {
+                item.Add();
+            }
+        }
+    }
+}
