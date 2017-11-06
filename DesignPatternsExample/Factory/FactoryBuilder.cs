f98083c3056a57e544076795e103f852b221014a
Vasilut <lucian.vasilut10@gmail.com>
2017-11-06 15:49:30 +0200

    Added Factory pattern.

---
+++ DesignPatternsExample/Factory/FactoryBuilder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    public class FactoryBuilder
+    {
+        public static ICar GetCar(string name)
+        {
+            ICar car = null;
+            switch (name)
+            {
+                case "bmw":
+                    {
+                        car = new Bmw();
+                        break;
+                    }
+                case "audi":
+                    {
+                        car = new Audi();
+                        break;
+                    }
+                default:
+                    {
+                        car = new Ferrari();
+                        break;
+                    }
+            }
+            return car;
+        }
+    }
+}
---
+++ DesignPatternsExample/Factory/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ICar obj = FactoryBuilder.GetCar("bmw");
+            Console.WriteLine(obj.GetSpeed());
+
+            string[] arr = new string[]{ "bmw","audi", "ferrari"};
+            foreach (var item in arr)
+            {
+                ICar objSearched = FactoryBuilder.GetCar(item);
+                Console.WriteLine(objSearched.GetSpeed());
+
+            }
+
+            Console.WriteLine("Hello from factory pattern");
+            Console.ReadLine();
+        }
+    }
+}
