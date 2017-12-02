db499e1d25ba1afc2b5ef8a84026d2162fba6db8
Vasilut Lucian <lucian.vasilut10@gmail.com>
2017-12-02 13:42:09 +0200

    Solved problem with AzureEntities.

--- ExcelToAzureDb/ExcelToAzureDb/Program.cs
+++ ExcelToAzureDb/ExcelToAzureDb/Program.cs
@@ -15,46 +15,46 @@ namespace ExcelToAzureDb
             //bag intr-un dictionar: cheie (nume elev)-> valoare clasa. dintr-un fisier initial.
 
 
-            //var excel = new ExcelQueryFactory(@"F:\Downloads\bnnm.xlsx");
-            //var data = (from c in excel.Worksheet<ModelToExport>("Clasament")
-            //            select c)?.ToList();
+            var excel = new ExcelQueryFactory(@"F:\Downloads\bnnm.xlsx");
+            var data = (from c in excel.Worksheet<ModelToExport>("Clasament")
+                        select c)?.ToList();
 
-            //foreach (ModelToExport item in data)
-            //{
-            //    Type type = item.GetType();
-            //    var propertyInfo = type.GetProperties();
+            foreach (ModelToExport item in data)
+            {
+                Type type = item.GetType();
+                var propertyInfo = type.GetProperties();
 
-            //    //add to cloud db the results
-            //    var resultToAdd = new Rezultate
-            //    {
-            //        Nume = item.Nume,
-            //        Prenume = item.Prenume,
-            //        Scoala = item.Scoala
-            //    };
+                //add to cloud db the results
+                var resultToAdd = new Rezultate
+                {
+                    Nume = item.Nume,
+                    Prenume = item.Prenume,
+                    Scoala = item.Scoala
+                };
 
-            //    //loop over all the properties
-            //    foreach (var prop in propertyInfo)
-            //    {
-            //        if (FactoryChooseClass.GetClasa("12").Contains(prop.Name))
-            //        {
-            //            object val = prop.GetValue(item, null); //valoarea
-            //            if (val != null)
-            //            {
-            //                AssignPointToModel.AssignProblemPoint(ref resultToAdd,val);
-            //            }
-            //        }
-            //    }
+                //loop over all the properties
+                foreach (var prop in propertyInfo)
+                {
+                    if (FactoryChooseClass.GetClasa("12").Contains(prop.Name))
+                    {
+                        object val = prop.GetValue(item, null); //valoarea
+                        if (val != null)
+                        {
+                            AssignPointToModel.AssignProblemPoint(ref resultToAdd, val);
+                        }
+                    }
+                }
 
-            //}
+            }
 
 
             //IRepository<Rezultate> repo = new LocalRepository();
             //repo.Add(new Rezultate { Clasa = 12, Nume = "Mock1", P1 = 30, P2 = 20, P3 = 15, Prenume = "PrenMock", Scoala = "Cnlr" });
             //repo.Save();
 
-            IRepository<Rezultate> repo = new AzureRepository();
-            repo.Add(new Rezultate { Clasa = 12, Nume = "Mock1", P1 = 30, P2 = 20, P3 = 15, Prenume = "PrenMock", Scoala = "Cnlr" });
-            repo.Save();
+            //IRepository<Rezultate> repo = new AzureRepository();
+            //repo.Add(new Rezultate { Clasa = 12, Nume = "Mock1", P1 = 30, P2 = 20, P3 = 15, Prenume = "PrenMock", Scoala = "Cnlr" });
+            //repo.Save();
 
             Console.WriteLine("Hello Luciiiii");
             Console.ReadLine();
