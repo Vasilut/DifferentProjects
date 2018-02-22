87986308eb0aa3dfd4c9ce3db8d962a370f6a153
Vasilut <lucian.vasilut10@gmail.com>
2018-02-22 15:49:49 +0200

    Added globomantics project.

---
+++ Globomantics/Globomantics/Services/ConferenceMemoryService.cs
@@ -0,0 +1,48 @@
+﻿using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Globomantics.Services
+{
+    public class ConferenceMemoryService:IConferenceService
+    {
+        private List<ConferenceModel> _conferences = new List<ConferenceModel>();
+        public ConferenceMemoryService()
+        {
+            _conferences.Add(new ConferenceModel { Id = 1, Name = "Luc1", Location = "Bucuresti", Start = DateTime.Now, AttendeeTotal = 9 });
+            _conferences.Add(new ConferenceModel { Id = 2, Name = "Luc2", Location = "Cluj", Start = DateTime.Now, AttendeeTotal = 15 });
+
+        }
+
+        public Task Add(ConferenceModel model)
+        {
+            model.Id = _conferences.Max(c => c.Id) + 1;
+            _conferences.Add(model);
+            return Task.CompletedTask;
+        }
+
+        public Task<IEnumerable<ConferenceModel>> GetAll()
+        {
+            return Task.Run(() => _conferences.AsEnumerable());
+        }
+
+        public Task<ConferenceModel> GetById(int id)
+        {
+            return Task.Run(() => _conferences.First(c => c.Id == id));
+        }
+
+        public Task<StatisticsModel> GetStatistics()
+        {
+            return Task.Run(() =>
+            {
+                return new StatisticsModel
+                {
+                    NumberOfAttendees = _conferences.Sum(c => c.AttendeeTotal),
+                    AverageConferenceAttendees = (int)_conferences.Average(c => c.AttendeeTotal)
+                };
+            });
+        }
+    }
+}
---
+++ Globomantics/Globomantics/Services/ProposalMemoryService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Shared.Models;
+
+namespace Globomantics.Services
+{
+    public class ProposalMemoryService : IProposalService
+    {
+        private List<ProposalModel> _proposals = new List<ProposalModel>();
+
+        public ProposalMemoryService()
+        {
+            _proposals.Add(new ProposalModel
+            {
+                Id = 1,
+                ConferenceId = 1,
+                Speaker = "Lucian",
+                Title = "Cenusareasa"
+            });
+
+            _proposals.Add(new ProposalModel
+            {
+                Id = 2,
+                ConferenceId = 2,
+                Speaker = "Lucian2",
+                Title = "Cenusareasa2"
+            });
+
+            _proposals.Add(new ProposalModel
+            {
+                Id = 3,
+                ConferenceId = 2,
+                Speaker = "Lucian2",
+                Title = "Cenusareasa2"
+            });
+        }
+        public Task Add(ProposalModel model)
+        {
+            model.Id = _proposals.Max(p => p.Id) + 1;
+            _proposals.Add(model);
+            return Task.CompletedTask;
+        }
+
+        public Task<ProposalModel> Approve(int proposalId)
+        {
+            return Task.Run(
+                () =>
+                {
+                    var proposal = _proposals.First(p => p.Id == proposalId);
+                    proposal.Approved = true;
+                    return proposal;
+                });
+        }
+
+        public Task<IEnumerable<ProposalModel>> GetAll(int conferenceId)
+        {
+            return Task.Run(() => _proposals.AsEnumerable());
+        }
+    }
+}
