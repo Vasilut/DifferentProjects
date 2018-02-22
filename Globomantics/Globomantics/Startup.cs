629da8f9b6b959b094169f279e8b60c58d4c6ee8
Vasilut <lucian.vasilut10@gmail.com>
2018-02-22 17:31:29 +0200

    Added middleware.

--- Globomantics/Globomantics/Startup.cs
+++ Globomantics/Globomantics/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Globomantics
 {
@@ -16,22 +17,29 @@ namespace Globomantics
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
             services.AddSingleton<IConferenceService, ConferenceMemoryService>();
             services.AddSingleton<IProposalService, ProposalMemoryService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,
+                              ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStatusCodePages();//show the error message on the page
+            app.UseStaticFiles(); //FOR images 
+            app.UseMvc(routes =>
+           {
+               routes.MapRoute(name: "default",
+                               template: "{controller=Conference}/{action=Index}/{id?}"
+                   );
+           });
         }
     }
 }
