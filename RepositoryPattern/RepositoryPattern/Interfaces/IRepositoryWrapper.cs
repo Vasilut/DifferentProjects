88fd897dc61d7ba9da85dc81d87843034b111a52
Vasilut Lucian <lucian.vasilut10@gmail.com>
2018-04-24 13:27:38 +0300

    Added RepositoryWrapper.

---
+++ RepositoryPattern/RepositoryPattern/Interfaces/IRepositoryWrapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryPattern.Interfaces
+{
+    public interface IRepositoryWrapper
+    {
+        IAccountRepository Account { get; }
+        IOwnerRepository Owner { get; }
+    }
+}
---
+++ RepositoryPattern/RepositoryPattern/RepositoryWrapper.cs
@@ -0,0 +1,53 @@
+﻿using Data;
+using RepositoryPattern.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryPattern
+{
+    public class RepositoryWrapper : IRepositoryWrapper
+    {
+
+        private RepoContext _repoContext;
+        private IOwnerRepository _owner;
+        private IAccountRepository _account;
+        private object lock1 = new object();
+        private object lock2 = new object();
+
+        public RepositoryWrapper(RepoContext context)
+        {
+            _repoContext = context;
+        }
+
+        public IAccountRepository Account
+        {
+            get
+            {
+                lock(lock1)
+                {
+                    if(_account == null)
+                    {
+                        _account = new AccountRepository(_repoContext);
+                    }                    
+                }
+                return _account;
+            }
+        }
+
+        public IOwnerRepository Owner
+        {
+            get
+            {
+                lock(lock2)
+                {
+                    if(_owner == null)
+                    {
+                        _owner = new OwnerRepository(_repoContext);
+                    }
+                }
+                return _owner;
+            }
+        }
+    }
+}
